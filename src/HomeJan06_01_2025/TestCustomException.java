2813430433e1cc6e9003afde0a0243371f58bc36
tasrifzaman <tasrifzaman12@gmail.com>
2026-01-05 23:29:22 +0600

    add 2 files

--- src/HomeJan06_01_2025/TestCustomException.java
+++ src/HomeJan06_01_2025/TestCustomException.java
@@ -1,14 +1,11 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package HomeJan06_01_2025;
 
-//
-
 import java.util.Scanner;
-
+/*
+Checked Exception : checked exception in compile-time.
+recoverable problem, file not found, database issue.
+eg : IOException, SQLException, ClassNotFoundException.
+*/
 public class TestCustomException {
     static class LowBalanceException extends Exception{
 
@@ -31,8 +28,8 @@ public class TestCustomException {
         //int currentBalance = 10000;
         //int withdrwalAmount = 6000;
         Scanner sc = new Scanner(System.in);
-        System.out.print("Enter CurrentBalance : ");
-        int currentBalance = sc.nextInt();
+         System.out.print("Enter CurrentBalance : ");
+         int currentBalance = sc.nextInt();
         
         System.out.print("Enter withdrwalAmount : ");
         int withdrwalAmount = sc.nextInt();
