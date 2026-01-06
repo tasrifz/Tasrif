13c509376f9ec1f8fee7946bfb449a6f65c5df9b
tasrifz <tasrifzaman12@gmail.com>
2026-01-06 15:53:51 +0600

    jan06

---
+++ src/Jan06/EmployeeService.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Jan06;
+
+/**
+ *
+ * @author Tasrif Zaman
+ */
+import java.util.Map;
+
+/**
+ *
+ * @author Tasrif Zaman
+ */
+public interface EmployeeService {
+
+    Map<Integer, Employee> getEmployees();
+}
