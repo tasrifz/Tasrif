ee59237e8ab0737d6d07ef813edcad35c05bdae7
tasrifzaman <tasrifzaman12@gmail.com>
2026-01-15 22:14:27 +0600

    jan 14

--- src/jan07/thread/ThreadTest.java
+++ src/jan07/thread/ThreadTest.java
@@ -1,46 +1,36 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package jan07.thread;
 
-/**
- *
- * @author TASRIF ZAMAN
- */
 public class ThreadTest {
     public static void main(String[] args) {
-       ThreadByClass[] threadsByClass = new ThreadByClass[3];  
+        // 1. Using Thread Class
+        ThreadByClass[] threadsByClass = new ThreadByClass[3];  
         for (int i = 0; i < threadsByClass.length; i++) {
             threadsByClass[i] = new ThreadByClass();
+            threadsByClass[i].setName("ClassThread-" + i);
             threadsByClass[i].start();
         }
 
-       Thread[] threadsByRunnable = new Thread[3];
+        // 2. Using Runnable Interface
+        Thread[] threadsByRunnable = new Thread[3];
         for (int i = 0; i < threadsByRunnable.length; i++) {
             Runnable r = new ThreadByInterface();
             threadsByRunnable[i] = new Thread(r);
+            threadsByRunnable[i].setName("RunnableThread-" + i);
             threadsByRunnable[i].start();
         }
     }
 }
 
-  class ThreadByClass extends Thread {
-     @Override
-       public void run() {
-         for (int i = 0; i < 5; i++) {
-            System.out.println("Thread Class: " + i);
-        }
+class ThreadByClass extends Thread {
+    @Override
+    public void run() {
+        System.out.println(Thread.currentThread().getName() + " is running...");
     }
 }
 
 class ThreadByInterface implements Runnable {
-     @Override
-       public void run() {
-          for (int i = 0; i < 5; i++) {
-            System.out.println("Runnable Interface: " + i);
-        }
+    @Override
+    public void run() {
+        System.out.println(Thread.currentThread().getName() + " (Runnable) is running...");
     }
 }
-
