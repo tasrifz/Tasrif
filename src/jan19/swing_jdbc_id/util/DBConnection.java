ed962e885e88c57b38b4b2433419e65061ab2511
tasrifzaman <tasrifzaman12@gmail.com>
2026-01-21 22:18:16 +0600

    jan20

--- src/jan19/swing_jdbc_id/util/DBConnection.java
+++ src/jan19/swing_jdbc_id/util/DBConnection.java
@@ -1,87 +1,71 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package jan19.swing_jdbc_id.util;
 
-import static jan07.employeemangement.util.DBConnection.getConnection;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
-import java.sql.Statement;
+
+import java.sql.*;
 import java.util.Scanner;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 
-
-
-/**
- *
- * @author Tasrif Zaman
- */
 public class DBConnection {
 
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        System.out.print("Enter your user ID: ");
-        int userId = sc.nextInt();
-        System.out.print("Enter your  User Name : ");
-        String userName = sc.next();
-        String sql = "INSERT INTO m_test(user_id, user_name) VALUES('"+userId+"','"+userName+"')";
+    public static void main(String[] args) throws ClassNotFoundException {
+        Class.forName("com.mysql.jdbc.Driver");
+        String url = "jdbc:mysql://localhost:3306/test";
+        String user = "root";
+        String password = "root";
+        Connection con = null;
         try {
-            Class.forName("com.mysql.jdbc.Driver");
-            String url = "jdbc:mysql://localhost:3306/test";
-            String user = "root";
-            String password = "root";
-            Connection con = DriverManager.getConnection(url, user, password);
-            Statement stmt = con.createStatement();
-            
-            int Status = stmt.executeUpdate(sql);
-               if (Status > 0) {
-                   System.out.println("Inserted");
              
-               } else {
-                   System.out.println("Fail");
-             }
-            
-        } catch (SQLException e) {
-            System.out.println(e);
-        } catch (Exception ex) {
-            System.out.println(ex);
-           
-        } 
-        
-        // -----------------UPDATE-----------------
-        System.out.print("Enter your user ID to update: ");
-         int userId1 = sc.nextInt();
-         sc.nextLine(); 
+            con = DriverManager.getConnection(url, user, password);
+     
+            // ---------------- INSERT ----------------
+            Scanner sc = new Scanner(System.in);
+            System.out.print("Enter your user ID: ");
+            int userId = sc.nextInt();
+            sc.nextLine();
 
-         System.out.print("Enter your new User Name: ");
-         String userName1 = sc.nextLine();
+            System.out.print("Enter your User Name: ");
+            String userName = sc.nextLine();
 
-         String sqlUpdate = "UPDATE m_test SET user_name = ? WHERE user_id = ?";
+            String insertSQL = "INSERT INTO m_test(user_id, user_name) VALUES (?, ?)";
+            PreparedStatement psInsert = con.prepareStatement(insertSQL);
+            psInsert.setInt(1, userId);
+            psInsert.setString(2, userName);
 
-         try (Connection con = getConnection();
-          PreparedStatement ps = con.prepareStatement(sqlUpdate)) {
-            
-            ps.setString(1, userName1); 
-            ps.setInt(2, userId1);     
+            if (psInsert.executeUpdate() > 0) {
+                System.out.println("Inserted Successfully");
+            }
 
-           int status = ps.executeUpdate();
+            // ---------------- UPDATE ----------------
+            System.out.print("Enter user ID to update: ");
+            int updateId = sc.nextInt();
+            sc.nextLine();
 
-           if (status > 0) {
-               System.out.println("Updated Successfully");
-           } else {
-               System.out.println("User ID not found");
-        }
+            System.out.print("Enter new User Name: ");
+            String newName = sc.nextLine();
 
-         }catch (SQLException ex) {
-              System.out.println(ex);;
+            String updateSQL = "UPDATE m_test SET user_name = ? WHERE user_id = ?";
+            PreparedStatement psUpdate = con.prepareStatement(updateSQL);
+            psUpdate.setString(1, newName);
+            psUpdate.setInt(2, updateId);
+
+            if (psUpdate.executeUpdate() > 0) {
+                System.out.println("Updated Successfully");
+            } else {
+                System.out.println("User ID not found");
+            }
+
+            // ---------------- GET ----------------
+            String selectSQL = "SELECT user_id, user_name FROM m_test";
+            PreparedStatement psSelect = con.prepareStatement(selectSQL);
+            ResultSet rs = psSelect.executeQuery();
+
+            System.out.println("\nUserID | UserName");
+            System.out.println("------------------");
+            while (rs.next()) {
+                System.out.println(rs.getInt("user_id") + " | " + rs.getString("user_name"));
+            }
+
         } catch (Exception e) {
-              System.out.println(e);;
+            e.printStackTrace();
         }
-
     }
 }
