e814443320b20213660128d9ebe2908b3e0aba80
tasrifz <tasrifzaman12@gmail.com>
2026-01-21 19:01:42 +0600

    jan21

---
+++ src/jan21/model/User.java
@@ -0,0 +1,53 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package jan21.model;
+
+/**
+ *
+ * @author Tasrif Zaman
+ */
+public class User {
+    private String username;
+    private String password;
+    private String name;
+    private int age;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    
+    
+    
+}
